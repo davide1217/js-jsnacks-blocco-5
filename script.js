d561befe3a033b541890ab19f569c204f64255ce
davide1217 <davidepizzola17@gmail.com>
2022-04-28 18:52:19 +0200

    jsnack-al-completo

--- script.js
+++ script.js
@@ -164,6 +164,9 @@ document.getElementById('sommaGrandi').innerHTML = `Il peso delle zucchine grand
 
 */
 
+
+/* jsnack3
+
 let parola = prompt('inserisci un parola');
 
 const controlloParola = (parola.includes(' ')) ? parola = prompt('inserisci UNA parola') : '' ;
@@ -174,3 +177,55 @@ const parolaGirata = giraParola(parola);
 
 document.getElementById('parola').innerHTML =  parola;
 document.getElementById('parolaGirata').innerHTML = parolaGirata;
+
+*/
+
+const biciclette = [
+  {
+    nome : 'freccia',
+    kg : 17
+  },
+  {
+    nome : 'scheggia',
+    kg : 15
+  },
+  {
+    nome : 'lampo',
+    kg : 20
+  },
+  {
+    nome : 'fulmine',
+    kg : 14
+  },
+]
+
+const biciPiuLeggera = () => {
+
+  let biciLeggera = '';
+
+  let pesoMinore = '';
+
+  for(let bicicletta of biciclette){
+
+    const {kg, nome} = bicicletta;
+
+    if(biciLeggera === '') {
+      biciLeggera = nome;
+      pesoMinore = kg;
+    } 
+    else if(pesoMinore > kg) {
+      pesoMinore = kg;
+      biciLeggera = nome;
+    }
+
+    console.log(biciLeggera);
+
+  }
+
+  return biciLeggera
+
+}
+
+let biciStampata = biciPiuLeggera();
+
+document.getElementById('biciPiuLeggera').innerHTML = `La bici più leggera è ${biciStampata}`;
